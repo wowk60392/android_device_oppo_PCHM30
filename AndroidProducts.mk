d22c0772cde6992128682552a6cccb83ef3fd5f7
wowk60392 <89296952+wowk60392@users.noreply.github.com>
2026-05-06 09:38:08 +0800

    update

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_PCHM30.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_PCHM30-user \
+    omni_PCHM30-userdebug \
+    omni_PCHM30-eng
